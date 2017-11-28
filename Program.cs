6ecd7bf1e1d0c65ce3f037e645a5ab3d0a44fe99
Ubuntu <ubuntu@alpha13.turing.ieduc.org.br>
2017-11-28 21:12:02 -0200

    versionamento

--- Program.cs
+++ Program.cs
@@ -1,13 +1,35 @@
 ﻿using System;
 
-namespace Brasileirao
+namespace app
 {
     class Program
     {
         static void Main(string[] args)
         {
-            TabelaBrasileirao tabela = new TabelaBrasileirao();
-            tabela.tabela();
+            Console.WriteLine("Tabela Brasileirão");
+            
+            ConsoleKeyInfo a;
+            Campeonato campeonato = new Campeonato();
+            do {
+                Console.Write("1 => nova equipe | 2 => resultado ");
+                Console.WriteLine("Digite sua opção: ");
+                a = Console.ReadKey();            
+                if (a.Key == ConsoleKey.D1) {
+                    System.Console.WriteLine("# Adicionando uma nova equipe");
+                    System.Console.WriteLine("Digite o nome da equipe: "); 
+                    string nome = Console.ReadLine();
+                    System.Console.WriteLine("Digite a pontuacao: "); 
+                    string pontuacao = Console.ReadLine();
+                    campeonato.equipes.Add(new Equipe(nome, Int32.Parse(pontuacao)));
+                }
+            } while (a.Key != ConsoleKey.D2);
+
+            System.Console.WriteLine("Listando o resultado");
+            campeonato.ordenaPontuacao();
+            campeonato.resultado();
+
+            System.Console.WriteLine(campeonato.equipes.Count);
+            
         }
     }
 }
